a9a1a3382379b24bb393fbfbce948237c902306e
hamim-ifty <hifty24@students.oamk.fi>
2024-10-08 15:52:21 +0300

    FInal

--- public/script.js
+++ public/script.js
@@ -6,12 +6,9 @@ function appendNumber(number) {
 }
 
 function appendOperator(operator) {
-  // Check if the last character is an operator
   if (input === '' || ['+', '-', '*', '/'].includes(input.slice(-1))) {
-    // If it's an operator, replace the last operator with the new one
     input = input.slice(0, -1) + operator;
   } else {
-    // Otherwise, append the operator as normal
     input += operator;
   }
   document.getElementById('result').value = input;
@@ -25,11 +22,10 @@ function clearScreen() {
 function calculate() {
   try {
     if (input) {
-      input = eval(input).toString(); // Evaluate the expression and convert to string
+      input = eval(input).toString();
       document.getElementById('result').value = input;
     }
   } catch (error) {
-    // If there's an error (like invalid input), show "Error"
     document.getElementById('result').value = 'Error';
     input = '';
   }
